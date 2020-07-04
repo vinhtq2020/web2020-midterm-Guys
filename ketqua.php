f3fc9bfd1b0bda6b5856670b6eb4cdfe8f9e08cb
HoangAnh <biminhiabo2@gmail.com>
2020-07-04 10:14:41 +0700

    xemdiem

---
+++ ketqua.php
@@ -0,0 +1,45 @@
+
+<?php
+include_once('DataProvider.php');
+?>
+<div >
+    <h3 class="text-center">DANH SACH SV</h3>
+    <table class="table table-bordered text-center" style="margin:0 auto;width:50%">
+        <tr>
+            <td>STT</td>
+            <td>Ten sv</td>
+            <td>Ten MH</td>
+            <td>Diem</td>
+            <td colspan="3"><a href="ThemSinhVien.php">Thêm</a></td>
+            
+        </tr>
+        <?php
+        
+        if(isset($_REQUEST['loaisv'])){
+            $loai=$_REQUEST['loaisv'];
+            $qr="SELECT * FROM `ketqua`,sinhvien,monhoc WHERE ketqua.MaSV=sinhvien.MaSV and ketqua.MaMH=monhoc.MaMH and sinhvien.MaSV=$loai";
+        }
+        else{
+            $qr="select * from sinhvien";
+        }
+
+        $result=DataProvider::ExecuteQuery($qr);
+
+        
+        
+
+        while($row=mysqli_fetch_array($result)){
+        ?>
+        <tr style="margin:20px !important">
+            <td><?php echo $row['MaSV'] ?></td>
+        
+            <td><?php echo $row['HoTen'] ?></td>
+            <td><?php echo $row['TenMH'] ?></td>
+            <td><?php echo $row['Diem'] ?></td>
+            <td></td>
+        </tr>
+        <?php
+        }
+        ?>
+    </table>
+</div>
---
+++ xemketqasv.php
@@ -0,0 +1,72 @@
+<html>
+<?php
+session_start();
+include_once('DataProvider.php');
+?>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<!-- Add the slick-theme.css if you want default styling -->
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+<!-- Add the slick-theme.css if you want default styling -->
+<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css" />
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="https://kit.fontawesome.com/e9a3bfa470.js" crossorigin="anonymous"></script>
+</head>
+<style>
+    table tr{
+        padding: 50px ;
+    }
+</style>
+<script>
+    
+        function laysvtheoloai(){
+            $.ajax({
+                url: 'ketqua.php',
+                type: 'post',
+                data: {
+                    'loaisv': $('#loaisv').val()
+                },
+                success: function(data){
+                    $('#dssv').html(data);
+
+                },
+                error: function(){
+                    alert("Lỗi xử lý");
+                }
+            });
+        }
+        
+        $(function(){
+            laysvtheoloai();
+            $('#loaisv').change(function(){
+                laysvtheoloai();
+            });
+        })
+    
+</script>
+<body>
+<div>
+Loai sv:
+
+<select name="loaisv" id="loaisv">
+    <?php
+    
+    $result_loai=DataProvider::ExecuteQuery('SELECT * FROM sinhvien');
+    while($row_loai=mysqli_fetch_array($result_loai)){
+    ?>
+    <option value="<?php echo $row_loai['MaSV'] ?>"><?php echo $row_loai['HoTen'] ?></option>
+    <?php
+    }
+    
+    ?>
+</select>
+
+</div>
+<div id="dssv">
+
+</div>
+
+</body>
+</html>
